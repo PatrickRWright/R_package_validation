bb0c8ecafce5b987680dd04517c703ff440bcdac
Patrick R. Wright <patrick.wright@usb.ch>
2020-01-24 16:44:35 +0100

    github issue to csv parser

---
+++ src/parse_issue_json_to_csv.R
@@ -0,0 +1,29 @@
+parse_issue_json_to_csv <- function(repos_owner,
+                                    repos_name,
+                                    outfile = paste0(getwd(), "/", "github_issues.csv")) {
+  # dependency
+  library(jsonlite)
+
+  link <- paste0("https://api.github.com/repos/", repos_owner, "/", repos_name, "/issues")
+  message(paste0("The link is: ", link))
+
+  # parse issues
+  issues <- fromJSON(link)
+  # retain relevant columns
+  issues_relevant <- issues[, c("title", "body", "created_at", "updated_at", "html_url")]
+
+  issue_labels_list <- issues$labels
+  # init
+  issue_labels <- c()
+  # this assumes that the order of the labels
+  # is in accordance to the order or issues
+  # thus labels can be assigned to issues by order
+  for (i in 1:length(issue_labels_list)) {
+    issue_labels <- c(issue_labels, paste(issue_labels_list[[i]]$name, collapse = ", "))
+  }
+  # output data frame
+  issues_csv <- cbind(issues_relevant, issue_labels)
+
+  message(paste0("Writing data to: ", outfile))
+  write.csv(x = issues_csv, file = outfile, quote = TRUE, row.names = FALSE)
+}
